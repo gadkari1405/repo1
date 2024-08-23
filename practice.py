21e754dcd146466a77c718f262c5d9dcf561b1ad
gadkari1405 <142561398+gadkari1405@users.noreply.github.com>
2024-08-23 18:51:01 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,17 @@
+# x="abcdefgh"           ( Access multiple element with  negative slicing)
+# print(x[-3:-6:-1])
+# print(x[-1:-4:-2])
+# print(x[-1:-6:-2])
+
+# (concatinating two strings)
+# var1="Hello"
+# var2="World"
+# print(var1,var2)
+
+# for checking data type.
+# z='rohan gadkari'
+# print(type(z))
+
+y='123456'
+print(len(y))
+
